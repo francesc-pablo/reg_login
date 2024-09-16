2e76fd923a01506dafeceb1767ce9e89485ef119
francesc-pablo <francescpacopablo@gmail.com>
2024-09-16 11:38:39 +0000

    first commit

---
+++ script/register.js
@@ -0,0 +1,10 @@
+// Select form by id
+const registerForm = document.getElementById('register-form');
+// Handle form submit event
+registerForm.onsubmit = function(){
+    // collect username
+    // collect email
+    // collect password
+    // save user information
+    // send a confirmation email
+}
